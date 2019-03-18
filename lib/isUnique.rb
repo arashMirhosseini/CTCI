e02232849b5ebf2adc1b15c75677bfbbfba3badd
Arash <arash.mh01@gmail.com>
2019-03-18 14:55:50 -0700

    Implement is unique by bit vector

--- lib/isUnique.rb
+++ lib/isUnique.rb
@@ -18,21 +18,12 @@ end
 
 def is_unique3(str)
   checker = 0
-  str.each_char do |char|
-    val = char.ord - 'a'.ord
-    return false if checker & (1 << val) != 0
-    checker |= 1 << val
-  end
-  checker.to_s(2)
-end
-
-def is_unique4(str)
-  hash = Hash.new(0)
   str.each_char do |c|
-    hash[c] += 1
-    return false if hash[c] > 1
+    index = c.ord - 'a'.ord
+    return false if checker & (1 << index) != 0
+    checker |= 1 << index
   end
   true
 end
 
-p is_unique4("helo")
+p is_unique3("hello")
