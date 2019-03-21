33ef8afaba203f2e3203159f137cdd31e9557300
Arash <arash.mh01@gmail.com>
2019-03-21 13:13:50 -0700

    Update urlify

--- lib/urlify.rb
+++ lib/urlify.rb
@@ -37,4 +37,28 @@ def urlify1(str, true_len)
   str
 end
 
-p urlify1('mr john smith    ', 13)
+def urlify2(str, true_length)
+  num_space = 0
+  (true_length - 1).downto(0) do |i|
+    if str[i] == ' '
+      num_space += 1
+    end
+  end
+  shift = num_space * 2
+  i = true_length - 1
+  while i >= 0
+    if str[i] != ' '
+      str[i+shift] = str[i]
+    else
+      str[i+shift] = '0'
+      str[i+shift-1] = '2'
+      str[i+shift-2] = '%'
+      shift -= 2
+    end
+
+    i -= 1
+  end
+  str
+end
+
+p urlify2(' hi mr john smith        ', 17)
