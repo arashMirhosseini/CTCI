442dff519b765f5c442e16ce7bae18594bc4ffc1
Arash <arash.mh01@gmail.com>
2019-03-21 14:02:10 -0700

    Update palindrome permut

--- lib/palindrome_permut.rb
+++ lib/palindrome_permut.rb
@@ -1,24 +1,23 @@
-def palindrome_permut(str)
-  count = 0
-  hash = Hash.new(0)
-  str = str.split(" ").join
+#Given a string, write a function to check if it 
+#is a permutation of a palindrome. A palindrome 
+#is a word or phrase that is the same forwards and 
+#backwards. A permutation is a rearrangement of letters.
+#The palindrome does not need to be limited to just 
+#dictionary words.
+
+def permutationOfPalindrome?(str)
+  hsh = Hash.new(0)
+  str = str.split(' ').join
+  len = str.size
   str.each_char do |c|
-    hash[c] += 1
+    hsh[c.downcase] += 1
   end
-
-  hash.each do |k,v|
-    if str.length % 2 == 0
-      return false if v != 2
-    else
-      if v == 1
-        count += 1
-        return false if count != 1
-      end
-      return false if v > 2
-    end
+  count = 0
+  hsh.each do |k, v|
+      count += 1 if v % 2 != 0
   end
-
+  return false if count > 1
   true
 end
 
-p palindrome_permut("tqact coqa")
+p permutationOfPalindrome?("aacccTTccaaaPP")
