d84b6c095e7d11c9d78886317e94a6da23af1060
Arash <arash.mh01@gmail.com>
2019-03-22 13:36:28 -0700

    Optimize string compression

--- lib/string_compression.rb
+++ lib/string_compression.rb
@@ -9,19 +9,19 @@
 def string_compression(str)
 
   count = 1
-  str_result = ""
+  str_result = []
   i = 0
   while i <= str.size-2
     if str[i] == str[i+1] 
       count += 1
     else
-      str_result += (str[i] + count.to_s)
+      str_result << (str[i] + count.to_s)
       count = 1
     end
     i += 1
   end
-  str_result += (str[i] + count.to_s)
-  (str.size <= str_result.size) ? (return str) : (return str_result)
+  str_result << (str[i] + count.to_s)
+  (str.size <= str_result.size) ? (return str) : (return str_result.join)
 end
 
 p string_compression('paabccccccaayyyyyy')
