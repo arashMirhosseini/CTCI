57953b305b33f6f51cb7703ec89d58c02126370e
Arash <arash.mh01@gmail.com>
2019-03-22 13:32:51 -0700

    Add string compression

--- lib/string_compression.rb
+++ lib/string_compression.rb
@@ -6,7 +6,22 @@
 #return the original string. You can assume the string 
 #has only uppercase and lowercase letters (a - z).
 
-def string_compression
-  hash = Hash.new(0)
-  
+def string_compression(str)
+
+  count = 1
+  str_result = ""
+  i = 0
+  while i <= str.size-2
+    if str[i] == str[i+1] 
+      count += 1
+    else
+      str_result += (str[i] + count.to_s)
+      count = 1
+    end
+    i += 1
+  end
+  str_result += (str[i] + count.to_s)
+  (str.size <= str_result.size) ? (return str) : (return str_result)
 end
+
+p string_compression('paabccccccaayyyyyy')
