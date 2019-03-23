78a725d9a5488cb64c0c68b4044205458626328d
Arash <arash.mh01@gmail.com>
2019-03-22 21:03:03 -0700

    Update zero matrix

--- lib/zero_matrix.rb
+++ lib/zero_matrix.rb
@@ -2,5 +2,31 @@
 # MxN matrix is 0, its entire row and column are set to 0.
 
 def zero_matrix(matrix)
-  
+  n = matrix.size
+  m = matrix[0].size
+  r = nil
+  c = nil
+  0.upto(n-1) do |i|
+    0.upto(m-1) do |j|
+      if matrix[i][j] == 0
+        r = i
+        c = j
+        break
+      end
+    end
+  end  
+
+  0.upto(n-1) do |i|
+    matrix[i][c] = 0
+  end
+
+  0.upto(m-1) do |i|
+    matrix[r][i] = 0
+  end
+  nil
 end
+
+matrix = [[1,2,0,4], [4,5,6,7], [8,9,1,2]]
+zero_matrix(matrix)
+
+p matrix
