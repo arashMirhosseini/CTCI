e891cd877653a7ebee42b91f145ba165c9fa6145
Arash <arash.mh01@gmail.com>
2019-03-23 18:01:53 -0700

    Complete first version of string rotation

---
+++ lib/string_rotation.rb
@@ -0,0 +1,27 @@
+# Assume you have a method isSubstring which checks if one 
+# word is a substring of another. 
+# Given two strings, s1 and s2, write code to check if s2 is 
+# a rotation of s1 using only one call to isSubstring 
+# (e.g., "waterbottle" is a rotation of" erbottlewat").
+
+# in Ruby, we can use include? method as isString method
+
+def string_rotation?(s1, s2)
+  return false unless s1.size == s2.size
+
+  i = 0
+  j = 0
+  while j < s2.size
+    if s1[i] == s2[j]
+      i += 1
+      j += 1
+    else
+      j += 1
+    end
+  end
+  index = j - i
+  return false unless s1.include? s2[0...index]
+  true
+end
+
+p string_rotation?('waterbottle', 'ottlewaterb')
