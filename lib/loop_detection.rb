7391cae484532a54cc839d8bdcd1fa055de1f13b
Arash <arash.mh01@gmail.com>
2019-03-28 13:46:34 -0700

    Update detect loops

--- lib/loop_detection.rb
+++ lib/loop_detection.rb
@@ -12,7 +12,27 @@ require_relative 'linked_list'
 # Input: A -> B -> C -> D -> E -> C[the same C as earlier]
 # Output: C
 
+def detect_loop?(l)
+  return false if l.nil? || l.next.nil? || l.next.next.nil?
+  
+  p1 = l
+  p2 = p1.next.next
+  n = 0
+  while p1 != p2
+    if p2.nil?
+      return false
+    end
+    p1 = p1.next
+    return false if p2.next.nil?
+    p2 = p2.next.next
+    # p n
+  end
+  true
+end
 
+def detect_loop(l)
+  
+end
 
 l = NodeList.new('A')
 l.append('B')
@@ -20,8 +40,10 @@ l.append('B')
 n1 = NodeList.new('C')
 l.append_link(n1)
 
-l2 = NodeList.new('D', 'E')
+l2 = NodeList.new('D')
+l2.append('E')
 n1.next = l2
-l2.last_node.next = n1
+l2.last_node.next = n1  
 
-puts l
+# puts l  
+p detect_loop(l)
