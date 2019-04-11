8b0a00a374aca1809fb2cd75b0dcce0aa15f4761
Arash <arash.mh01@gmail.com>
2019-04-10 21:43:39 -0700

    Add fib method

--- lib/fib.rb
+++ lib/fib.rb
@@ -29,4 +29,32 @@ def fib(n)
   fib[n-1]
 end
 
-p fib(5)
+# fib(n) = fib(n-1) + fib(n-2)
+# fib = 1 if n < 3
+# fib(3) = 1 + 1 = 2
+# fib(4) = 2 + 1 = 3
+# fib(5) = 3 + 2 = 5
+
+def fib1(n)
+  return 1 if n < 3
+  # fib1(n-1) + fib1(n-2)
+  fibs = [1, 1, 1]
+  i = 3
+  while i <= n
+    fibs << fibs[i-1] + fibs[i-2]
+    i += 1
+  end
+  fibs[n]
+end
+
+# Count ways to reach the n’th stair
+
+def count_nth(n)
+  # return n if n < 2
+  # count_nth(n-1) + count_nth(n-2)
+  fib1(n+1)
+end
+
+# p fib1(5)
+p count_nth(4)
+
