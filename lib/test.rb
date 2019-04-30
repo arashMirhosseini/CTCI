6c223f4118fb4568a10ceeb70582b2fa6f1e336e
Arash <arash.mh01@gmail.com>
2019-04-30 12:38:55 -0700

    Update

--- lib/test.rb
+++ lib/test.rb
@@ -1,11 +1,24 @@
-class Dog
-  attr-accessor :name, :color, :age
-  def initialize(name, color, age)
-    @name = name
-    @color = color 
-    @age = age
+nums1 = [1, 3, 4, 5]
+nums2 = []
+
+def merge(nums1, nums2)
+  res = []
+  i = 0
+  j = 0
+  while !nums1.empty? && !nums2.empty?
+    if nums1[0] <= nums2[0]
+      res << nums1.shift
+    else
+      res << nums2.shift
+    end
+  end
+  if !nums1.empty? 
+    res + nums1
+  elsif !nums2.empty?
+    res + nums2
+  else
+    res
   end
 end
 
-dog1 = Dog.new('tommy', 'brown', 3)
-dog2 = Dog.new('foxy', 'white', 2)
+p merge(nums1, nums2)
