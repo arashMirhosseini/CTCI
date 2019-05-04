ae3121fe35df3ef35169e6ea86c447128728aea9
Arash <arash.mh01@gmail.com>
2019-05-04 15:25:28 -0700

    Add heap

--- lib/budget_cut.rb
+++ lib/budget_cut.rb
@@ -8,19 +8,26 @@
 # to newBudget).
 
 def find_grants_cap(grantsArray, newBudget)
-  each = newBudget.to_f / grantsArray.size
-  remainder = 0.0
-  n = 0
-  grantsArray.each do |grant|
-    if grant < each 
-      n += 1
-      remainder += each - grant
-    end
+  grantsArray.sort!
+  sum_curr_budget = 0
+  i = 0
+  while i < grantsArray.size
+    sum_curr_budget += grantsArray[i]
+    break if sum_curr_budget > newBudget
+    i += 1
   end
-  p remainder
-  cap = remainder.to_f / (grantsArray.size - n)
-  p cap
-  return cap == 0 ? each : (cap + each)
+  
+  if sum_curr_budget < newBudget
+    return grantsArray.max
+  end
+
+  num_surplus_recepient = grantsArray.size - i 
+
+  (newBudget - (sum_curr_budget - grantsArray[i])).to_f / num_surplus_recepient
+
+
+
+
 end
 
 
@@ -29,9 +36,13 @@ end
 # Expected output:
 # 128.0
 
+# grantsArray = [210,200,150,193,130,110,209,342,117] 
+# newBudget = 1530
+# expected output: 211
+
 grantsArray = [21,100,50,120,130,110]
 newBudget = 140
-# Expected:
+# Expected output:
 # 23.8
 
 p find_grants_cap(grantsArray, newBudget)
--- lib/k_messed_array_sort.rb
+++ lib/k_messed_array_sort.rb
@@ -22,14 +22,36 @@ def insersion_sort(arr)
   i = 1
   while i < arr.size
     j = i
-    while j >= 0 && arr[j] < arr[j-1]
+    while j > 0 && arr[j] < arr[j-1]
+      puts "i: #{i}, j: #{j}"
+      puts "arr: #{arr}"
       arr[j], arr[j-1] = arr[j-1], arr[j]
+      puts "arr (after): #{arr}"
       j -= 1
     end
     i += 1
+   
   end
-  p arr
+  arr
 end
 # p sort_k_messed_array(arr, k)
-insersion_sort(arr)
+# insersion_sort(arr)
 # p arr
+nums = [2, 0, 1]
+# nums = [2,0,2,1,1,0]
+# p insersion_sort(nums)
+
+require "rubygems"
+require "algorithms"
+
+include Containers
+
+input = [1,1,1,2,2,3] 
+k = 2
+
+def heap_most_frequent(arr, num = 1)
+  max_heap = MaxHeap.new(arr.group_by(&:itself).map{|n,ns| [ns.count,n]})
+  (1..num).each_with_object([]) { |i, result| result << max_heap.pop[1]}
+end
+
+heap_most_frequent(nums)
