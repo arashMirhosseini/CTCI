a5d6341f14396d61075487e2b38caf455e19328c
Arash <arash.mh01@gmail.com>
2019-05-06 11:19:36 -0700

    Add

---
+++ lib/smallest_substring.rb
@@ -0,0 +1,10 @@
+# Smallest Substring of All Characters
+# Given an array of unique characters arr and a string str, 
+# Implement a function getShortestUniqueSubstring that finds the 
+#smallest substring of str containing all the characters in arr. 
+#Return "" (empty string) if such a substring doesn’t exist.
+
+# input:  arr = ['x','y','z'], str = "xyyzyzyx"
+
+# output: "zyx"
+
