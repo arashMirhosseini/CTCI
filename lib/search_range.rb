b79469829040ad00b9ef8d0f3aeb138fb9fb1316
Arash <arash.mh01@gmail.com>
2019-05-15 12:32:13 -0700

    Complete search ranges

--- lib/search_range.rb
+++ lib/search_range.rb
@@ -3,5 +3,34 @@
 # @return {Integer[]}
 
 def search_range(nums, target)
-  
+  i = 0
+  j = nums.size - 1
+  res = Array.new(2){-1}
+  # find the left side
+  while i < j
+    mid = (i + j) / 2
+    if nums[mid] == target
+      j = mid
+    elsif nums[mid] > target 
+      j = mid
+    else
+      i = mid + 1
+    end
+  end
+  return res if nums[i] != target 
+  res[0] = i
+
+  # find the right side
+  while i < j
+    mid = (i + j) / 2 
+    if nums[mid] == target 
+      i = mid
+    elsif nums[mid] < target
+      i = mid
+    else
+      j = mid - 1
+  end
+  res[1] = j
+  res
 end
+
